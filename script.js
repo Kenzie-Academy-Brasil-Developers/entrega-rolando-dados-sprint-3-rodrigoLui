f6176146a12b89333926587170596c15c426641d
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-01 22:35:09 -0300

    inicial commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+
+
+    function test() {
+        const rolarDados = () => {
+            let dado1 = Math.floor(Math.random() * 6 + 1)
+            let dado2 = Math.floor(Math.random() * 6 + 1)
+            let soma = dado1 + dado2
+            return soma
+        }
+
+        let frequencia = [0]
+
+        for (let i = 0; i < 100; i++) {
+            frequencia.push(rolarDados())
+        }
+
+        return frequencia
+    }
+
