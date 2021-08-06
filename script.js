6de7d2f1a7cfb762394adbe29d653bd639d7caea
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-06 11:18:10 -0300

    inical commit

--- script.js
+++ script.js
@@ -2,18 +2,36 @@
 
 
     function test() {
-        const rolarDados = () => {
-            let dado1 = Math.floor(Math.random() * 6 + 1)
-            let dado2 = Math.floor(Math.random() * 6 + 1)
-            let soma = dado1 + dado2
-            return soma
-        }
+        
+        const botao = document.getElementById('bnt')
+        botao.addEventListener('click', function () {
+            
+            const rolarDados = () => {
+                let dado1 = Math.floor(Math.random() * 6 + 1)
+                let dado2 = Math.floor(Math.random() * 6 + 1)
+                let soma = dado1 + dado2
+                return soma
+            }
+            let arrayTest = []
+            for (let i = 0; i < 100; i++) {
+                
+                let valDados = rolarDados()
+                arrayTest.push(valDados)
+            }
+            
+            const box = document.getElementById('box')
+            const div = document.createElement('div')
+            
+            for (let i = 0; i < array.length; i++) {
+                
+                const element = array[i];
+                
+            }
+    
 
-        let frequencia = [0]
 
-        for (let i = 0; i < 100; i++) {
-            frequencia.push(rolarDados())
-        }
+
+        })
 
         return frequencia
     }
