3904a4d04225d239458cf6a59361b21f6e1ca115
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-13 20:21:42 -0300

    testando

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 
-
-
-    function test() {
-        
-        const botao = document.getElementById('bnt')
-        botao.addEventListener('click', function () {
-            
-            const rolarDados = () => {
-                let dado1 = Math.floor(Math.random() * 6 + 1)
-                let dado2 = Math.floor(Math.random() * 6 + 1)
-                let soma = dado1 + dado2
-                return soma
-            }
-            let arrayTest = []
-            for (let i = 0; i < 100; i++) {
-                
-                let valDados = rolarDados()
-                arrayTest.push(valDados)
-            }
-            
-            const box = document.getElementById('box')
-            const div = document.createElement('div')
-            
-            for (let i = 0; i < array.length; i++) {
-                
-                const element = array[i];
-                
-            }
+function test() {
     
 
+    let arrayTest = []
 
+        const rolarDados = () => {
+            let dado1 = Math.floor(Math.random() * 6 + 1)
+            let dado2 = Math.floor(Math.random() * 6 + 1)
+            let soma = dado1 + dado2
+            return soma
+        }
+        for (let i = 0; i < 100; i++) {
+            let aux = rolarDados()
 
-        })
+            if (arrayTest[i] === null) {
+                arrayTest.push(aux)                
+            }
+            
+        }
 
-        return frequencia
-    }
+        console.log(arrayTest)
+}
 
