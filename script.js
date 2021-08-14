1936a525382bd07da2ad699869180104d24c2594
Rodrigo Luiz <79553015+rodrigoLui@users.noreply.github.com>
2021-08-13 21:57:45 -0300

    concluído

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-
-function test() {
+function colocaNaTela() {
     
 
-    let arrayTest = []
+
+
+
+    const bnt = document.getElementById('bnt')
+    bnt.addEventListener('click', function () {
+        let count = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
         const rolarDados = () => {
             let dado1 = Math.floor(Math.random() * 6 + 1)
@@ -10,15 +14,23 @@ function test() {
             let soma = dado1 + dado2
             return soma
         }
-        for (let i = 0; i < 100; i++) {
-            let aux = rolarDados()
 
-            if (arrayTest[i] === null) {
-                arrayTest.push(aux)                
-            }
-            
+        for (let i = 0; i < 1000; i++) {
+            let rollOfDice = rolarDados()
+            count[rollOfDice] = count[rollOfDice] + 1
         }
 
-        console.log(arrayTest)
-}
+        const box = document.getElementById('box')
 
+        for (let i = 2; i <= 12; i++) {
+            const div = document.createElement('div')
+            div.setAttribute('id', `miniBox${i}`)
+            div.innerText = `${i}:${count[i]}`
+            div.style.width = `${count[i] + 50}px`
+            div.style.height = '30px'
+            div.style.backgroundColor = 'grey'
+            box.appendChild(div)
+        }
+        
+    })
+} colocaNaTela()
